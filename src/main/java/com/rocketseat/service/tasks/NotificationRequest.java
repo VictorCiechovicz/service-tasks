9ac8453365e5953530ff1a9ac47c6760038950d6
Victor <victor09ciechovicz@outlook.com>
2025-12-29 18:52:28 -0300

    feat:create NotificationClient and NotificationSchedule

---
+++ src/main/java/com/rocketseat/service/tasks/NotificationRequest.java
@@ -0,0 +1,5 @@
+package com.rocketseat.service.tasks;
+
+import java.time.LocalDateTime;
+
+public record NotificationRequest(String message, String email) {}
--- src/main/java/com/rocketseat/service/tasks/TasksEntity.java
+++ src/main/java/com/rocketseat/service/tasks/TasksEntity.java
@@ -1,9 +1,6 @@
 package com.rocketseat.service.tasks;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +8,8 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Entity
+@Entity(name = "Task")
+@Table(name = "task")
 @Getter
 @Setter
 @AllArgsConstructor
@@ -21,7 +19,7 @@ public class TasksEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
+    private String email;
     private String title;
     private LocalDateTime dueDate;
     private boolean notified;
